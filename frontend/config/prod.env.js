2feb277e1ecbcf84eec321752d7fa5420fcd5ebb
Justin Headley <jheadley@archsys.io>
2019-10-28 14:25:19 -0700

    refactor: prod URIs

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_URI: '"https://api.appyapp.io"',
-  WEBSOCKET_URI: '"wss://api.appyapp.io"',
-  APP_URI: '"https://demo.appyapp.io"'
-}
+  SERVER_URI: '"https://mbt.guide.api.appyapp.io"',
+  WEBSOCKET_URI: '"wss://mbt.guide.api.appyapp.io"',
+  APP_URI: '"https://mbt-guide.netlify.com/"',
+};
